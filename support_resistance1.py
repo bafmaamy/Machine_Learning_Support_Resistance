764cb906c87b36e28565aa28fa23c34129c1ed6b
n0nstart <80202580+bafmaamy@users.noreply.github.com>
2022-10-06 23:04:27 +0300

    Update support_resistance1.py

--- support_resistance1.py
+++ support_resistance1.py
@@ -9,20 +9,23 @@ from sklearn.metrics import silhouette_score
 
 # ----------------- inputs ---------------------
 # Valid intervals: [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo]
-interval = "1mo"
+interval = "1h"
+
+# number of clusters +1
+numK = 7
 
 # K factor 
 # more about K-value - https://datascience.stackexchange.com/questions/75789/why-is-10-considered-the-default-value-for-k-fold-cross-validation
 # 10 is considered as default value 
 # Basically if you have enough data, the factor can be lowered - https://datascience.stackexchange.com/questions/75789/why-is-10-considered-the-default-value-for-k-fold-cross-validation
-n_init_input = 5
+n_init_input = 8
 
 # ticker
-symbol = 'TLRY'
+symbol = 'ACB'
 
 # --- calculate based on selected timeframe
-# Valid periods: [1mo,1wk,5m,3mo,6mo,1y,Max]
-period = "Max"
+# Valid periods: [1mo,1wk,5m,3mo,6mo,1y,5y,Max]
+period = "4mo"
 # -----------------------------------------------
 
 def load_prices(ticker, per, interval):
@@ -31,9 +34,18 @@ def load_prices(ticker, per, interval):
     return data.dropna()
 
 def get_optimum_clusters(df, saturation_point=0.05):
+    '''
+    :param df: dataframe
+    :param saturation_point: The amount of difference we are willing to detect
+    :return: clusters with optimum K centers
+    
+    This method uses elbow method to find the optimum number of K clusters
+    We initialize different K-means with 1..10 centers and compare the inertias
+    If the difference is no more than saturation_point, we choose that as K and move on
+    '''
     wcss = []
     k_models = []
-    size = min(11, len(df.index))
+    size = min(numK, len(df.index))
     for i in range(1, size):
         kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=n_init_input, random_state=None)
         kmeans.fit(df)
@@ -102,7 +114,7 @@ print(f'indicator("My script", overlay=true)')
 x = datetime.datetime.now()
 nametitlet = x.strftime("%d-%m-%Y")
 
-file_object = open(symbol+str(nametitlet)+".txt", 'a+')
+file_object = open(symbol+str(nametitlet)+str(interval)+str(period)+str(n_init_input)+".txt", 'a+')
 zline = '//@version=5'
 fline = 'indicator("SR", overlay=true)'
 sline = "plot(close)"
@@ -110,7 +122,7 @@ file_object.write(zline+"\n")
 file_object.write(fline+"\n")
 file_object.write(sline+"\n")
 for i in range(a):
-    a = 'hline('+str(lowss[y])+', title='+'"'+interval+'_'+symbol+'_support'+'"'', color=color.rgb(0, 238, 40), linestyle=hline.style_dotted)'
+    a = 'hline('+str(lowss[y])+', title='+'"'+interval+'_'+symbol+'_support'+'"'', color=color.rgb(56, 146, 71), linestyle=hline.style_solid, linewidth=2)'
     # insert supports into the file
     file_object.write(a+"\n")
     print(a)
@@ -120,7 +132,7 @@ for i in range(a):
 
 z = 0
 for i in range(b):
-    b = 'hline('+str(highss[z])+', title='+'"'+interval+'_'+symbol+'_resistance'+'"'', color=color.rgb(192, 48, 48), linestyle=hline.style_dotted)'
+    b = 'hline('+str(highss[z])+', title='+'"'+interval+'_'+symbol+'_resistance'+'"'', color=color.rgb(192, 48, 48), linestyle=hline.style_solid, linewidth=2)'
     # insert resistances into the file
     file_object.write(b+"\n")
     print(b)
